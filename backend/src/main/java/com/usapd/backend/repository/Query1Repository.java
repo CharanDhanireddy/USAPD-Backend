176989edc897c85621d35b0dc86b6b28796fc42c
CharanDhanireddy <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>
2021-12-02 22:15:19 -0500

    Modified queries for datestr

--- backend/src/main/java/com/usapd/backend/repository/Query1Repository.java
+++ backend/src/main/java/com/usapd/backend/repository/Query1Repository.java
@@ -12,50 +12,7 @@ import java.util.List;
 @Repository
 public interface Query1Repository extends JpaRepository<Test, Integer> {
 
-        @Query(value = "SELECT dc.year," +
-                "       dc.week," +
-                "       Avg(ap.arithmetic_mean) as meanValue " +
-                "FROM   (" +
-                "              SELECT *" +
-                "              FROM   vdhavaleswarapu.observation o where( o.pollutant_code =" +
-                "                     (" +
-                "                            SELECT pollutant_code" +
-                "                            FROM   vdhavaleswarapu.pollutant p" +
-                "                            WHERE  (" +
-                "                                          p.pollutant_name = :pollutant ) ) )" +
-                "              AND    (" +
-                "                            o.site_code IN" +
-                "                            (" +
-                "                                   SELECT *" +
-                "                                   FROM   (" +
-                "                                                 SELECT site_code" +
-                "                                                 FROM   vdhavaleswarapu.state state" +
-                "                                                 JOIN   vdhavaleswarapu.county county" +
-                "                                                 ON     county.state_code = state.state_code" +
-                "                                                 JOIN   vdhavaleswarapu.site site" +
-                "                                                 ON     site.county_code = county.county_code" +
-                "                                                 WHERE  state_name = :state )))) ap " +
-                "JOIN" +
-                "       (" +
-                "              SELECT dc.date_id," +
-                "                     dc.year," +
-                "                     (dc.month" +
-                "                            || '/'" +
-                "                            || dc.day" +
-                "                            || '/'" +
-                "                            || dc.year) AS date_str," +
-                "                     to_char(to_date(dc.month" +
-                "                            || '/'" +
-                "                            || dc.day" +
-                "                            || '/'" +
-                "                            || dc.year, 'MM/DD/YYYY'), 'WW') AS week from vdhavaleswarapu.datecollected dc" +
-                "                   WHERE TO_DATE(dc.month || '/' || dc.day || '/' || dc.year, 'MM/DD/YYYY')" +
-                "                   BETWEEN TO_DATE(:startDate, 'MM/DD/YYYY')" +
-                "                       AND TO_DATE(:endDate, 'MM/DD/YYYY') ) dc " +
-                "ON     ap.date_id = dc.date_id " +
-                "       group BY dc.year, dc.week " +
-                "       order BY dc.year, dc.week",
+        @Query(value = "SELECT dateData.year, dateData.week, Avg(ap.arithmetic_mean) AS meanValue FROM (SELECT * FROM vdhavaleswarapu.observation o where(o.pollutant_code = (SELECT pollutant_code FROM vdhavaleswarapu.pollutant p WHERE (p.pollutant_name = :pollutant) )) AND (o.site_code IN (SELECT * FROM (SELECT site_code FROM vdhavaleswarapu.state state JOIN vdhavaleswarapu.county county ON county.state_code = state.state_code JOIN vdhavaleswarapu.site site ON site.county_code = county.county_code WHERE state_name = :state )))) ap JOIN (SELECT dc.date_id, dc.year, to_char(dc.datestr, 'WW') AS WEEK FROM vdhavaleswarapu.datecollected dc WHERE datestr BETWEEN :startDate AND :endDate ) dateData ON ap.date_id = dateData.date_id GROUP BY dateData.year, dateData.week ORDER BY dateData.year, dateData.week",
             nativeQuery = true)
     List<JSONObject> getPollutantDataByState(String pollutant, String state, String startDate, String endDate);
-
 }
--- backend/src/main/resources/queries/Q5.sql
+++ backend/src/main/resources/queries/Q5.sql
@@ -1,69 +1,70 @@
-  (SELECT MONTH,
-          YEAR,
-          count(avgAQI) AS DAYCOUNT
-   FROM
-     (SELECT dateData.year,
-             dateData.date_id,
-             dateData.month,
-             avg(aqiData.aqi) avgAQI
-      FROM
-        (SELECT date_id,
-                aqi
-         FROM VDHAVALESWARAPU.observation o
-         WHERE (o.site_code in
-                  (SELECT *
-                   FROM
-                     (SELECT site_code
-                      FROM VDHAVALESWARAPU.State state
-                      JOIN VDHAVALESWARAPU.County county ON county.state_code = state.state_code
-                      JOIN VDHAVALESWARAPU.Site site ON site.county_code = county.county_code
-                      WHERE state_name = :state)))) aqiData,
 
-        (SELECT dc.date_id,
-                dc.year,
-                dc.month
-         FROM VDHAVALESWARAPU.datecollected dc
-         WHERE TO_DATE(dc.month || '/' || dc.day || '/' || dc.year, 'MM/DD/YYYY') BETWEEN TO_DATE(:startDate, 'MM/DD/YYYY') AND TO_DATE(:endDate, 'MM/DD/YYYY')) dateData
-      WHERE aqiData.date_id = dateData.date_id
-        AND aqiData.aqi != 0
-      GROUP BY dateData.year,
-               dateData.month,
-               dateData.date_id)
-   WHERE avgAQI >= :threshold
-   GROUP BY MONTH,
-            YEAR)
+(SELECT MONTH,
+     YEAR,
+     count(avgAQI) AS DAYCOUNT
+ FROM
+     (SELECT dateData.year,
+     dateData.date_id,
+     dateData.month,
+     avg(aqiData.aqi) avgAQI
+     FROM
+     (SELECT date_id,
+     aqi
+     FROM VDHAVALESWARAPU.observation o
+     WHERE (o.site_code in
+     (SELECT *
+     FROM
+     (SELECT site_code
+     FROM VDHAVALESWARAPU.State state
+     JOIN VDHAVALESWARAPU.County county ON county.state_code = state.state_code
+     JOIN VDHAVALESWARAPU.Site site ON site.county_code = county.county_code
+     WHERE state_name = :state)))) aqiData,
+
+     (SELECT dc.date_id,
+     dc.year,
+     dc.month
+     FROM VDHAVALESWARAPU.datecollected dc
+     WHERE dc.datestr BETWEEN :startDate AND :endDate) dateData
+     WHERE aqiData.date_id = dateData.date_id
+     AND aqiData.aqi != 0
+     GROUP BY dateData.year,
+     dateData.month,
+     dateData.date_id)
+ WHERE avgAQI >= :threshold
+ GROUP BY MONTH,
+     YEAR)
 UNION
-  (SELECT MONTH,
-          YEAR,
-          0
-   FROM
+(SELECT MONTH,
+     YEAR,
+     0
+ FROM
      (SELECT dateData.year,
-             dateData.date_id,
-             dateData.month,
-             avg(aqiData.aqi) avgAQI
-      FROM
-        (SELECT date_id,
-                aqi
-         FROM VDHAVALESWARAPU.observation o
-         WHERE (o.site_code in
-                  (SELECT *
-                   FROM
-                     (SELECT site_code
-                      FROM VDHAVALESWARAPU.State state
-                      JOIN VDHAVALESWARAPU.County county ON county.state_code = state.state_code
-                      JOIN VDHAVALESWARAPU.Site site ON site.county_code = county.county_code
-                      WHERE state_name = :state)))) aqiData,
+     dateData.date_id,
+     dateData.month,
+     avg(aqiData.aqi) avgAQI
+     FROM
+     (SELECT date_id,
+     aqi
+     FROM VDHAVALESWARAPU.observation o
+     WHERE (o.site_code in
+     (SELECT *
+     FROM
+     (SELECT site_code
+     FROM VDHAVALESWARAPU.State state
+     JOIN VDHAVALESWARAPU.County county ON county.state_code = state.state_code
+     JOIN VDHAVALESWARAPU.Site site ON site.county_code = county.county_code
+     WHERE state_name = :state)))) aqiData,
 
-        (SELECT dc.date_id,
-                dc.year,
-                dc.month
-         FROM VDHAVALESWARAPU.datecollected dc
-         WHERE TO_DATE(dc.month || '/' || dc.day || '/' || dc.year, 'MM/DD/YYYY') BETWEEN TO_DATE(:startDate, 'MM/DD/YYYY') AND TO_DATE(:endDate, 'MM/DD/YYYY')) dateData
-      WHERE aqiData.date_id = dateData.date_id
-        AND aqiData.aqi != 0
-      GROUP BY dateData.year,
-               dateData.month,
-               dateData.date_id)
-   GROUP BY MONTH,
-            YEAR
-   HAVING max(avgAQI) < :threshold)
+     (SELECT dc.date_id,
+     dc.year,
+     dc.month
+     FROM VDHAVALESWARAPU.datecollected dc
+     WHERE dc.datestr BETWEEN :startDate AND :endDate) dateData
+     WHERE aqiData.date_id = dateData.date_id
+     AND aqiData.aqi != 0
+     GROUP BY dateData.year,
+     dateData.month,
+     dateData.date_id)
+ GROUP BY MONTH,
+     YEAR
+ HAVING max(avgAQI) < :threshold);
