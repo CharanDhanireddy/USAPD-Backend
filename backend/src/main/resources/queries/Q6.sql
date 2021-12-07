a38044da32fc8ab0932add65d60b04122e5e0ef6
vdhavaleswarapu <89587982+vdhavaleswarapu@users.noreply.github.com>
2021-12-07 14:42:42 -0500

    Q6

--- backend/src/main/resources/queries/Q6.sql
+++ backend/src/main/resources/queries/Q6.sql
@@ -1,57 +1,58 @@
 SELECT
-    ROWNUM AS RANK,
-    STATE,
-    AQI AS AVGAQI
+   ROWNUM AS RANK,
+   STATE,
+   AQI AS AVGAQI 
 FROM
-    (
-        SELECT
-            TO_CHAR(AQIDATA.DATE_STR2, 'YYYY') AS YEAR,
-            SITESTATE.STATE_NAME AS STATE,
-            AVG(AQIDATA.AQI) AS AQI
-        FROM
-            (
-                SELECT
-                    AQI,
-                    DATE_STR2,
-                    SITE_CODE
-                FROM
-                    VDHAVALESWARAPU.OBSERVATION O
-                WHERE
-                    (
-                            O.SITE_CODE IN
-                            (
-                                SELECT
-                                    SITE_CODE
-                                FROM
-                                    VDHAVALESWARAPU.STATE STATE
-                                        JOIN
-                                    VDHAVALESWARAPU.COUNTY COUNTY
-                                    ON COUNTY.STATE_CODE = STATE.STATE_CODE
-                                        JOIN
-                                    VDHAVALESWARAPU.SITE SITE
-                                    ON SITE.COUNTY_CODE = COUNTY.COUNTY_CODE
-                            )
-                        )
-            )
-                AQIDATA
-                JOIN
-            (
-                SELECT
-                    SITE_CODE,
-                    STATE_NAME
-                FROM
-                    VDHAVALESWARAPU.STATE S
+   (
+      SELECT
+         TO_CHAR(AQIDATA.DATE_STR2, 'YYYY') AS YEAR,
+         SITESTATE.STATE_NAME AS STATE,
+         AVG(AQIDATA.AQI) AS AQI 
+      FROM
+         (
+            SELECT
+               AQI,
+               DATE_STR2,
+               SITE_CODE 
+            FROM
+               VDHAVALESWARAPU.OBSERVATION O 
+            WHERE
+               (
+                  O.SITE_CODE IN 
+                  (
+                     SELECT
+                        SITE_CODE 
+                     FROM
+                        VDHAVALESWARAPU.STATE STATE 
                         JOIN
-                    VDHAVALESWARAPU.SITE SI
-                    ON S.STATE_CODE = SI.STATE_CODE
+                           VDHAVALESWARAPU.COUNTY COUNTY 
+                           ON COUNTY.STATE_CODE = STATE.STATE_CODE 
+                        JOIN
+                           VDHAVALESWARAPU.SITE SITE 
+                           ON SITE.COUNTY_CODE = COUNTY.COUNTY_CODE 
+                  )
+               )
+         )
+         AQIDATA 
+         JOIN
+            (
+               SELECT
+                  SITE_CODE,
+                  STATE_NAME 
+               FROM
+                  VDHAVALESWARAPU.STATE S 
+                  JOIN
+                     VDHAVALESWARAPU.SITE SI 
+                     ON S.STATE_CODE = SI.STATE_CODE 
             )
-                SITESTATE
-            ON SITESTATE.SITE_CODE = AQIDATA.SITE_CODE
-        WHERE
-                TO_CHAR(AQIDATA.DATE_STR2, 'YYYY') = '2020'
-        GROUP BY
-            SITESTATE.STATE_NAME,
-            TO_CHAR(AQIDATA.DATE_STR2, 'YYYY')
-        ORDER BY
-            AQI
-    );
+            SITESTATE 
+            ON SITESTATE.SITE_CODE = AQIDATA.SITE_CODE 
+      WHERE
+         TO_CHAR(AQIDATA.DATE_STR2, 'YYYY') = '2020' 
+      GROUP BY
+         SITESTATE.STATE_NAME,
+         TO_CHAR(AQIDATA.DATE_STR2, 'YYYY') 
+      ORDER BY
+         AQI 
+   )
+;
